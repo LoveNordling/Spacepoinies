d88265cc9b41ef1e76f0fcc19c2ee2365d91f9f0
Martin Björling <mabj@snowfall.se>
2017-06-02 21:36:24 +0200

    Adding project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spacePoniesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spacePoniesApp
+ */
+angular.module('spacePoniesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
