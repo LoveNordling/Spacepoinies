69454be988cf612b8ce0c7c0fefd918f66ab8063
LoveNordling <love_nordling@hotmail.com>
2017-06-03 16:41:49 +0200

    fix bug

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -11,18 +11,16 @@ angular.module('spacePoniesApp')
   .controller('UserCtrl', function ($scope, $http, CONFIG, $rootScope, $location) {
     if (typeof $rootScope.user === 'undefined') {
       $location.path('/');
+    } else {
+      $http({
+        method: 'POST',
+        url: CONFIG.API_URL + 'get_user',
+        data: {'token': $rootScope.user.token},
+        headers: {'Content-Type': 'application/json'}
+      }).then(function successCallback(response) {
+
+      }, function errorCallback(response) {
+
+      });
     }
-
-    $http({
-      method: 'POST',
-      url: CONFIG.API_URL + 'get_user',
-      data: {'token': $rootScope.user.token},
-      headers: {'Content-Type': 'application/json'}
-    }).then(function successCallback(response) {
-
-    }, function errorCallback(response) {
-
-    });
-
-
   });
