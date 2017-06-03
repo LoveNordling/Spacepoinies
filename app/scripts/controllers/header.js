21fbb9486f6c58c742b5e7d5eb1548b4098637c8
Martin Björling <mabj@snowfall.se>
2017-06-03 12:08:52 +0200

    latest

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -8,17 +8,45 @@
  * Controller of the spacePoniesApp
  */
 angular.module('spacePoniesApp')
-  .controller('HeaderCtrl', function ($scope, $rootScope, $http) {
+  .controller('HeaderCtrl', function ($scope, $rootScope, $http, CONFIG) {
     $scope.userForm = {
       'username': '',
       'password': ''
     };
 
     $scope.login = function() {
-      $http.post(API_URL + '/login', $scope.userForm).then(function(res) {
+      console.log("hej");
+
+      $http({
+        method: 'POST',
+        url: CONFIG.API_URL + 'login',
+        data: JSON.stringify($scope.userForm),
+        headers: {'Content-Type': 'application/json'}
+      }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+
+        console.log(response)
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log(response)
+      });
+
+      /*$http.post(CONFIG.API_URL, {'text':$scope.userForm.username}).success(function(res) {
+        console.log(res);
+      }).error(function(res) {
+        console.log(res);
+      });*/
+
+      /*$http.post(CONFIG.API_URL + 'login', $scope.userForm).success(function(res) {
+        console.log(res);
+
         $rootScope.user = res;
         $rootScope.hasLoggedIn = true;
-      });
+      }).error(function(res) {
+        console.log(res);
+      });*/
     };
 
     $scope.logout = function() {
