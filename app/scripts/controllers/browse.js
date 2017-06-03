4a1909e7eff0ee4f39cc332006810a750ad57152
Martin Björling <mabj@snowfall.se>
2017-06-03 23:25:24 +0200

    sadnhgasdd

--- app/scripts/controllers/browse.js
+++ app/scripts/controllers/browse.js
@@ -9,5 +9,8 @@
  */
 angular.module('spacePoniesApp')
   .controller('BrowseCtrl', function ($scope, $http, CONFIG, $rootScope) {
-
+    $scope.clickStupidButton = function() {
+      document.body.className = 'transform';
+      setTimeout(function(){ document.body.className = ''; },75);
+    }
   });
