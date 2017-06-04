a9cddec50a41de22bd6f421940aea378d3669ca9
Martin Björling <mabj@snowfall.se>
2017-06-04 09:53:11 +0200

    sjdgyasd

--- app/scripts/controllers/match.js
+++ app/scripts/controllers/match.js
@@ -8,8 +8,16 @@
  * Controller of the spacePoniesApp
  */
 angular.module('spacePoniesApp')
-  .controller('MatchCtrl', function ($scope, $http, CONFIG, $rootScope, $location) {
+  .controller('MatchCtrl', function ($scope, $http, CONFIG, $rootScope, $location, $timeout) {
     $scope.clicked = false;
+    $scope.changed = false;
+
+    let countUp = function() {
+        $scope.changed = true;
+    }
+
+    $timeout(countUp, 200);
+
 
     $scope.clickButton = function() {
       $scope.clicked = true;
